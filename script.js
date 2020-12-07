5e5c5e0bcb8e7f233d22100c3e0aabbdda3ed5df
patterncard <patcad@o2.pl>
2020-12-07 14:00:33 +0100

    objects

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-// addition
-let a = 1000;
-let b = a + 200;
+// let player = new Object();
+let player = {
+    name: 'Your Name', 
+    energy: 100, 
+    lives: 2, 
+    x: 19, 
+    y: 100, 
+    color: 'blue'};
 
-alert(b);
+console.log(player);
+console.log(player.name);
+console.log(player['color']);
+
+player.name = 'some other name';
+console.log(player.name);
+
+player.friend = {name: 'Pablo'};
+player.weapon = 'sword';
